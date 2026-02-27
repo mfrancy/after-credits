89b083f3d12238e6e34e23900c637427d288c965
Mariana <marianalopes697@gmail.com>
2026-02-27 15:39:14 -0300

    style: estilização do banner

--- src/app/components/banner/banner.ts
+++ src/app/components/banner/banner.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [MenubarModule, IconFieldModule, InputIconModule, InputTextModule, Button],
   templateUrl: './banner.html',
   styleUrl: './banner.css',
 })
-export class Banner {
+export class Banner implements OnInit {
+
+  itemsMenu: MenuItem[] | undefined
+
+  ngOnInit(): void {
+    this.itemsMenu = [
+      {
+        icon: 'pi pi-bell'
+      },
+      {
+        icon: 'pi pi-user'
+      }
+    ]
+  }
 
 }
