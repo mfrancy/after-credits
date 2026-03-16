b0d137e80c53e20305caa7a10187420fd42be53d
Mariana <marianalopes697@gmail.com>
2026-03-16 14:35:25 -0300

    feat: cria model para a página de um filme

---
+++ src/app/models/movie-details.model.ts
@@ -0,0 +1,13 @@
+export interface MovieDetail {
+    adult: boolean;
+    backdrop_path: string;
+    belongs_to_collection: string;
+    genres: [];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    poster_path: string;
+    release_date: string;
+    title: string;
+}
